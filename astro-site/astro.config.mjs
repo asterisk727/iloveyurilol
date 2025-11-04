e8c29130c706135c0a179d3f4c4024c2087bf9ac
asterisk727 <59166650+asterisk727@users.noreply.github.com>
2025-11-04 13:29:57 -0500

    refactor: clean up css to be more consistent

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -7,8 +7,6 @@ import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
-
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
